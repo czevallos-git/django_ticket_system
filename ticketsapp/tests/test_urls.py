e5b1208becb591d2b21e9e6485d18f37b30a24ef
Cristhian Zevallos <cristhian.zevallos@sophos.com>
2023-07-04 15:27:18 +0100

    add url tests

---
+++ ticketsapp/tests/test_urls.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from ticketsapp.views import home_page
+from ticketsapp.auth import login_page, register_page
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_is_resolved(self):
+        url = reverse('home_page')
+        self.assertEquals(resolve(url).func, home_page)
+
+    def test_login_url_is_resolved(self):
+        url = reverse('login_page')
+        self.assertEquals(resolve(url).func, login_page)
+    
+    def test_register_url_is_resolved(self):
+        url = reverse('register_page')
+        self.assertEquals(resolve(url).func, register_page)
