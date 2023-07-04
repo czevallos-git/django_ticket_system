a4e2a8f1505c82ee934d4cf929bf9890f5ae41b5
Cristhian Zevallos <cristhian.zevallos@sophos.com>
2023-07-04 15:28:28 +0100

    rename url tests

--- ticketsapp/tests/test_urls.py
+++ ticketsapp/tests/test_urls.py
@@ -6,14 +6,14 @@ from ticketsapp.auth import login_page, register_page
 
 class TestUrls(SimpleTestCase):
 
-    def test_home_url_is_resolved(self):
+    def test_home_url_is_resolves(self):
         url = reverse('home_page')
         self.assertEquals(resolve(url).func, home_page)
 
-    def test_login_url_is_resolved(self):
+    def test_login_url_is_resolves(self):
         url = reverse('login_page')
         self.assertEquals(resolve(url).func, login_page)
     
-    def test_register_url_is_resolved(self):
+    def test_register_url_is_resolves(self):
         url = reverse('register_page')
         self.assertEquals(resolve(url).func, register_page)
